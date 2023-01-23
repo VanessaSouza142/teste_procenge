7643234b9bed680ab381c835a785da625005d343
VanessaSouza142 <vanessa.kbs@hotmail.com>
2023-01-23 00:28:51 -0300

    commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent,
+    TaskFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/tasks/task-list/task-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { VirtualTimeScheduler } from 'rxjs';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+
+export class TaskListComponent {
+
+}
+
